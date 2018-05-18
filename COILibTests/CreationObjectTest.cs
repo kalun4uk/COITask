afd366cd77fc0545e47413bcaa87f271c3ce7fdf
kalun4uk <34134819+kalun4uk@users.noreply.github.com>
2018-05-18 17:23:21 +0300

    Add files via upload

---
+++ COILibTests/CreationObjectTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using COILibraryPoint;
+
+namespace COILibTests
+{
+    [TestClass]
+    public class CreationObjectTest
+    {
+        [TestMethod]
+        public void TypeOfObjectDownCast()
+        {
+            Point<int> point = new Point1D<int>(1);
+            var actual = point.GetType();
+            var expected = typeof(Point1D<int>);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TypeOfObjectWithoutDownCast()
+        {
+            Point<int> point = new Point1D<int>(1);
+            var actual = point.GetType();
+            var expected = typeof(Point<int>);
+            Assert.AreNotSame(expected, actual);
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidCastException))]
+        public void InvalidTypeCast()
+        {
+            Point<int> point = new Point1D<int>(1);
+            var obj = (PMCDataModel<int>)(PMCDataCreator<double>)point;
+            PMCDataCreator<int> pointCreatorInt = new PositionCreator<int>();
+            pointCreatorInt.FactoryMethod().Add(obj);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ArgumentNull()
+        {
+            PMCDataCreator<int> pointCreatorInt = new PositionCreator<int>();
+            pointCreatorInt.FactoryMethod().Add(null);
+        }
+
+    }
+}
---
+++ COILibTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("COILibTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EPAM Systems")]
+[assembly: AssemblyProduct("COILibTests")]
+[assembly: AssemblyCopyright("Copyright © EPAM Systems 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("752d40dc-b4f5-44f3-9f0f-90cee59d472c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
